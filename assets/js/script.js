fe5af4bfb599383b92196563732c83d5f69605b0
tanyaleepr <tanyaleepr@gmail.com>
2022-01-23 19:10:34 -0600

    apikeys, jQuery, functions for weather forcast

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    //Global Variables Defined
+    var apikey = "a1127dc4ba5ff83b7283d877dfb9a775"; //API Key for using weather App
+    var units = "imperial"; //Set the units to come back as Farenheit
+  //Set JQuery Event Handler for click event on Search Button
+  $("#searchBtn").on("click", function () {
+    // Get the value from the id=search input field
+    var cityValue = $("#search").val();
+
+    // Clear input box
+    $("#search").val("");
+
+    //Call weatherForcast function using the search value
+    weatherForcast(cityValue);
+  });
+ // Set JQuery Event Handler for any list item in history and then call the weatherForcast function
+ $("#history-list").on("click", "li", function () {
+    var historyValue = $(this).text(); //Grab the value of the history item
+    weatherForcast(historyValue); //call the seatherForcast function with the histroy Value
+  });
